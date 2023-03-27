a25d5d6e61edfcd42063d4d3a30b2deb6f922fda
shamim <shamimice03@gmail.com>
2023-03-27 02:24:38 +0000

    user updated

--- examples/vpc-sg-rds-mysql/main.tf
+++ examples/vpc-sg-rds-mysql/main.tf
@@ -39,3 +39,61 @@ resource "aws_security_group" "rds_security_group" {
     cidr_blocks = ["172.16.0.0/16"]
   }
 }
+
+
+module "rds-blueprint" {
+  source  = "shamimice03/rds-blueprint/aws"
+  
+  # DB Subnet Group
+  create_db_subnet_group = true
+  db_subnet_group_name = "db-subnet-group"
+  db_subnets = module.vpc.db_subnet_id
+  
+  # Identify DB instance
+  db_identifier = "test-db-1"
+
+  # Create Initial Database
+  db_name = "mydb"
+  
+  # Credentials Settings
+  db_master_username = "Admin"
+  db_master_password = "Superadmin123"
+  iam_database_authentication_enabled = true
+  
+  # Availability and durability
+  multi_az = false
+  
+  # Engine options
+  engine = "mysql"
+  engine_version = "8.0"
+  
+  # DB Instance configurations
+  instance_class = "db.t3.micro"
+  
+  # Storage
+  storage_type = "gp2"
+  allocated_storage = "10"
+  max_allocated_storage = "10"
+  
+  # Connectivity
+  db_security_groups = [
+    "sg-00dd287a4b2efc40c"
+  ]
+  publicly_accessible = false
+  database_port = 3306
+  
+  # Backup and Maintenance
+  backup_retention_period = 7
+  backup_window = "03:00-05:00"
+  maintenance_window = "Sat:05:00-Sat:07:00"
+  deletion_protection = false
+  
+  # Monitoring
+  enabled_cloudwatch_logs_exports = ["audit", "error"]
+  
+  # Others
+  apply_immediately = true
+  delete_automated_backups = true
+  skip_final_snapshot = true
+
+}
